39943e0720d5b5327a7db2fa9c421caf1b86630b
maheshwar2499 <maheshwar2499@gmail.com>
2022-04-27 20:35:17 +0530

    Implemented movement, jumping, and dashing of player character

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject mainCamera;
+
+    Vector3 playerVelocity;
+    Vector3 playerJump;
+
+    float playerSpeed = 2.0f;
+    float jumpSpeed = 10.0f;
+    float gravity = 5.5f;
+
+    bool isGrounded = true;
+    bool jump = false;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +28,66 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //horizontal move
+        playerVelocity += transform.right * playerSpeed * Time.deltaTime;
+        playerVelocity = Vector3.ClampMagnitude(playerVelocity, 10f);
+        playerJump = jumpSpeed * transform.up * Time.deltaTime;
+        if (Input.GetKey("d"))
+        {
+            player.transform.position += playerVelocity;
+        }
+        if (Input.GetKey("a"))
+        {
+            player.transform.position -= playerVelocity;
+        }
+        playerVelocity -= playerVelocity * 0.2f;
+
+        //jump and fall
+        if (player.transform.position.y > 1)
+
+            isGrounded = false;
+        else
+            isGrounded = true;
+
+        if (Input.GetKeyDown("w"))
+        {
+            if (isGrounded == true)
+            {
+                jump = true;
+                isGrounded = false;
+            }
+        }
+
+        if (jump == true)
+        {
+            player.transform.position += playerJump;
+            if (player.transform.position.y > 5)
+            {
+                jump = false;
+            }
+        }
+        if (isGrounded == false)
+        {
+            if (jump == false)
+                player.transform.position += gravity * Time.deltaTime * -transform.up;
+        }
+
+        //dash
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+
+            player.transform.position += new Vector3(2, 0, 0);
+        }
+
+        //camera position
+        Vector3 pos = mainCamera.transform.position;
+        pos.x = player.transform.position.x;
+        mainCamera.transform.position = pos;
+
+        //correct player y position
+        if (player.transform.position.y < 1)
+        {
+            player.transform.position = new Vector3(player.transform.position.x, 1, player.transform.position.z);
+        }
     }
 }
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float playerSpeed;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] GameObject mainCamera;
+
+    float movePlayer;
+
+    [SerializeField] Rigidbody2D rb;
+
+    public bool isJumping;
+
+    [SerializeField] float dashSpeed = 14f;
+    [SerializeField] float dashTime = 0.4f;
+    Vector2 dashDirection;
+    bool isDashing;
+    bool canDash;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // player move
+        movePlayer = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(playerSpeed * movePlayer, rb.velocity.y);
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, 10f);
+
+        //player jump
+        if((Input.GetButtonDown("Jump")) && (isJumping == false))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpSpeed));
+        }
+
+        // player dash
+        if((Input.GetButtonDown("Dash")) && canDash)
+        {
+            isDashing = true;
+            canDash = false;
+
+            dashDirection = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+            StartCoroutine(StopDash());
+        }
+
+        if(isDashing)
+        {
+            rb.velocity = Vector2.zero;
+            rb.velocity = dashDirection * dashSpeed;
+            return;
+        }
+
+        // camera follow player
+        Vector3 pos = mainCamera.transform.position;
+        pos.x = rb.transform.position.x;
+        mainCamera.transform.position = pos;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Floor"))
+        {
+            isJumping = false;
+            canDash = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Floor"))
+        {
+            isJumping = true;
+           // canDash = false;
+        }
+    }
+
+    IEnumerator StopDash()
+    {
+        yield return new WaitForSeconds(dashTime);
+        isDashing = false;
+        canDash = true;
+    }
+}
