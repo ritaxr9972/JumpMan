8fa39bb6f9ea3548a0d8aab07957e862bf1080e2
maheshwar2499 <maheshwar2499@gmail.com>
2022-06-06 11:16:33 +0530

    Revert "Revert "Revert "Added new shooting enemy"""
    This reverts commit 6d810351471e931d3402567e9613c1421cec7221.

--- Assets/Scripts/ShootEnemy.cs
+++ Assets/Scripts/ShootEnemy.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class ShootEnemy : MonoBehaviour
 {
-    [SerializeField] GameObject enemyProjectile;
-    [SerializeField] GameObject spawnPoint;
-    [SerializeField] float spawnTime;
-    bool canSpawn = true;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -18,43 +13,6 @@ public class ShootEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (canSpawn)
-        {
-            StartCoroutine(SpawnProjectile());
-        }
-    }
-
-    IEnumerator SpawnProjectile()
-    {
-        canSpawn = false;
-        Instantiate(enemyProjectile, spawnPoint.transform.position, Quaternion.identity);
-        yield return new WaitForSeconds(spawnTime);
-        canSpawn = true;
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        foreach(ContactPoint2D pos in collision.contacts)
-        {
-            if(pos.normal.x > 0)
-            {
-                if(collision.gameObject.name == "Player")
-                {
-                    if(collision.gameObject.GetComponent<Movement>().isSliding)
-                    {
-                        gameObject.SetActive(false);
-                    }
-                    else
-                    {
-                        UnityEditor.EditorApplication.isPlaying = false;
-                    }
-                }
-
-            }
-            else
-            {
-                UnityEditor.EditorApplication.isPlaying = false;
-            }
-        }
+        
     }
 }
