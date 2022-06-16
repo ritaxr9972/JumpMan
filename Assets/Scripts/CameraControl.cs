2df9ce5009b7826f7c57f0c610ee512e2971ceba
maheshwar2499 <maheshwar2499@gmail.com>
2022-06-16 16:24:46 +0530

    Made the camera movement smooth

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -7,8 +7,11 @@ public class CameraControl : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] float cameraOffset;
     [SerializeField] float offsetSmoothing;
+   // [SerializeField] float smoothTime;
     Rigidbody2D rb;
     Vector3 pos;
+  //  Vector3 vel = Vector3.zero;
+   // bool isCameraChaged = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,20 +20,28 @@ public class CameraControl : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+
+        
+
         pos = new Vector3(rb.transform.position.x, rb.transform.position.y, transform.position.z);
 
         if (!player.GetComponent<SpriteRenderer>().flipX)
         {
             pos = new Vector3(pos.x + cameraOffset, pos.y, pos.z);
+            
         }
         else if (player.GetComponent<SpriteRenderer>().flipX)
         {
             pos = new Vector3(pos.x - cameraOffset, pos.y, pos.z);
+            
         }
+        
 
-        transform.position = Vector3.Lerp(transform.position, pos, offsetSmoothing * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, pos, offsetSmoothing * Time.deltaTime);
+        transform.position = Vector3.Lerp(transform.position, pos, offsetSmoothing * Time.fixedDeltaTime);
+      // transform.position = Vector3.SmoothDamp(transform.position, pos, ref vel, smoothTime * Time.deltaTime);
     }
 
     
