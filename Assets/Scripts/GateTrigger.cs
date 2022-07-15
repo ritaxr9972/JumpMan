9e85961acfac7a22019a0452dae70be65afa4dd4
Akshat Rawat <axr9972@rit.edu>
2022-07-15 14:08:28 -0400

    script fix

--- Assets/Scripts/GateTrigger.cs
+++ Assets/Scripts/GateTrigger.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class GateTrigger : MonoBehaviour
 {
-    [SerializeField] GameObject player; 
-    gameObject.SetActive(false);
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject gateTrigger;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.x<gameObject.transform.position && player.transform.position.y > gameObject.transform.position.y)
+
+        if (player.transform.position.x < gameObject.transform.position.x)
         {
             gameObject.SetActive(true);
         }
-           
     }
 
     
